59ae765a25bf5c2e78a2828d0328022f4f48983b
louis_maury <louis3340@gmail.com>
2020-05-14 16:05:38 +0000

    :lipstick: Navbar et footer... CHECK !

--- release/src/Controller/HomeController.php
+++ release/src/Controller/HomeController.php
@@ -2,18 +2,42 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleCategoryRepository;
+use App\Repository\StoryCategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private $storyCategoryRepository;
+    private $articleCategoryRepository;
+
+    public function  __construct(StoryCategoryRepository $storyCategoryRepository, ArticleCategoryRepository $articleCategoryRepository)
+    {
+        $this->storyCategoryRepository = $storyCategoryRepository;
+        $this->articleCategoryRepository = $articleCategoryRepository;
+    }
+
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
     public function index()
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
+    }
+
+    public function navBar()
+    {
+        return $this->render('partials/navbar.html.twig', [
+            'story_category' => $this->storyCategoryRepository->findAll(),
+            'article_category' => $this->articleCategoryRepository->findAll()
+        ]);
+    }
+
+    public function footer()
+    {
+        return $this->render('partials/footer.html.twig');
     }
 }
